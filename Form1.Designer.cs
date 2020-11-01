20f190f36286e58056b4be92721531fa6f754185
stein4444 <eysmont.vitya@gmail.com>
2020-11-01 23:46:50 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Forms_Second_Hw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LbTextScore = new System.Windows.Forms.Label();
+            this.LbScore = new System.Windows.Forms.Label();
+            this.PiKochka = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.PiGontel = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PiKochka)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PiGontel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LbTextScore
+            // 
+            this.LbTextScore.BackColor = System.Drawing.SystemColors.Control;
+            this.LbTextScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LbTextScore.Location = new System.Drawing.Point(12, 9);
+            this.LbTextScore.Name = "LbTextScore";
+            this.LbTextScore.Size = new System.Drawing.Size(111, 40);
+            this.LbTextScore.TabIndex = 0;
+            this.LbTextScore.Text = "Score";
+            // 
+            // LbScore
+            // 
+            this.LbScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LbScore.Location = new System.Drawing.Point(142, 9);
+            this.LbScore.Name = "LbScore";
+            this.LbScore.Size = new System.Drawing.Size(84, 40);
+            this.LbScore.TabIndex = 1;
+            // 
+            // PiKochka
+            // 
+            this.PiKochka.BackgroundImage = global::Forms_Second_Hw.Properties.Resources.kochka;
+            this.PiKochka.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PiKochka.Location = new System.Drawing.Point(136, 237);
+            this.PiKochka.Name = "PiKochka";
+            this.PiKochka.Size = new System.Drawing.Size(90, 70);
+            this.PiKochka.TabIndex = 2;
+            this.PiKochka.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // PiGontel
+            // 
+            this.PiGontel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.PiGontel.BackgroundImage = global::Forms_Second_Hw.Properties.Resources.gontel;
+            this.PiGontel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PiGontel.Location = new System.Drawing.Point(664, 246);
+            this.PiGontel.Name = "PiGontel";
+            this.PiGontel.Size = new System.Drawing.Size(55, 61);
+            this.PiGontel.TabIndex = 3;
+            this.PiGontel.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Forms_Second_Hw.Properties.Resources.kochalka;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PiGontel);
+            this.Controls.Add(this.PiKochka);
+            this.Controls.Add(this.LbScore);
+            this.Controls.Add(this.LbTextScore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.PiKochka)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PiGontel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LbTextScore;
+        private System.Windows.Forms.Label LbScore;
+        private System.Windows.Forms.PictureBox PiKochka;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox PiGontel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms_Second_Hw
+{
+    public partial class Form1 : Form
+    {
+        private int heigh = 1024;
+        private int width = 768;
+        private int size { get; set; }
+        int score = 0;
+        int stepMin = 2;
+        int stepMax = 5;
+        int x, y;
+        Point gontel;
+        public Form1()
+        {
+            InitializeComponent();
+            size = 60;
+            this.Text = "Kochalochka";
+            this.Width = width;
+            this.Height = heigh;
+            this.LbScore.Text += score.ToString();
+            
+        }
+        private void GenerateGantel()
+        {
+            this.PiGontel.Size = new Size(size,size);
+            this.PiGontel.Visible = true;
+            Random rand = new Random();
+            gontel.X = rand.Next(0, heigh - size);
+            int tempX = gontel.X % size;
+            gontel.X -= tempX;
+            gontel.Y = rand.Next(0, heigh - size);
+            int tempY = gontel.Y % size;
+            gontel.Y -= tempY;
+            gontel.X++;
+            gontel.Y++;
+            this.PiGontel.Location = new Point(gontel.X, gontel.Y);
+            this.Controls.Add(PiGontel);
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (PiKochka.Location.X == gontel.X && PiKochka.Location.Y == gontel.Y)
+            {
+                GenerateGantel();
+                this.score += 50;
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.Left:
+                    PiKochka.Location = new Point(PiKochka.Location.X - size, PiKochka.Location.Y);
+                    break;
+                case Keys.Up:
+                    PiKochka.Location = new Point(PiKochka.Location.X , PiKochka.Location.Y- size);
+                    break;
+                case Keys.Right:
+                    PiKochka.Location = new Point(PiKochka.Location.X + size, PiKochka.Location.Y);
+                    break;
+                case Keys.Down:
+                    PiKochka.Location = new Point(PiKochka.Location.X, PiKochka.Location.Y+size);      
+                    break;
+                case Keys.Oemplus:
+                    size += 10;
+                    break;
+                case Keys.OemMinus:
+                    size -= 10;
+                    break;
+                case Keys.Control:
+                    break;
+                default:
+                    break;
+            }
+            this.Refresh();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void GameOver()
+        {
+            if(score >= 400)
+            {
+                DialogResult window = MessageBox.Show($"Congratulation - You Win\nYou want play again?" + MessageBoxButtons.YesNo);
+                switch (window)
+                {
+                    case DialogResult.Yes:
+
+                        break;
+                    case DialogResult.No:
+                        this.Close();
+                        break;
+                }
+            }
+        }
+    }
+}
